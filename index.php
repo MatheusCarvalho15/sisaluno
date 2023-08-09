7649a82037a45a7b4900b088dabf489deaf01ee0
Matheus de Carvalho <129870244+MatheusCarvalho15@users.noreply.github.com>
2023-08-09 10:19:16 -0300

    index.php

--- index.php
+++ index.php
@@ -17,13 +17,13 @@
             <h3>Cadastros</h3>
         <button class="button"><a href="../aluno/cadaluno.php">cadastrar Aluno</a></button>
         <button class="button"><a href="../professor/cadprofessor.php">cadastrar Professor</a></button>
-        <button class="button"><a href="../Disciplina/cadDisciplina.php">Cadastrar Disciplina</a></button>
+        <button class="button"><a href="../disciplina/cadDisciplina.php">Cadastrar Disciplina</a></button>
         </div>
         <div class="Listas">
             <h3>Listas</h3>
         <button class="button"><a href="../aluno/listaalunos.php">lista de Alunos</a></button>
         <button class="button"><a href="../professor/listaprofessor.php">lista de Professores</a></button>
-        <button class="button"><a href="../Disciplina/listaDisciplina.php">lista de Disciplina</a></button>
+        <button class="button"><a href="../disciplina/listaDisciplina.php">lista de Disciplina</a></button>
         </div>
        
     </div>
